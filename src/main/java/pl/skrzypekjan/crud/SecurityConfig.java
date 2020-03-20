98cd3978b98082b123e399f3f1db114791b785ab
Jan Skrzypkowski <js.skrzypkowski@gmail.com>
2020-03-20 16:03:45 +0100

    security config modification

--- src/main/java/pl/skrzypekjan/crud/SecurityConfig.java
+++ src/main/java/pl/skrzypekjan/crud/SecurityConfig.java
@@ -1,34 +1,39 @@
 package pl.skrzypekjan.crud;
 
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation
         .web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation
         .web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import pl.skrzypekjan.crud.Security.SpringDataUserDetailsService;
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    DataSource dataSource;
+
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user1").password("user123").roles("USER")
-                .and()
-                .withUser("admin1").password("admin123").roles("ADMIN");
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .passwordEncoder(passwordEncoder());
     }
 
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/admin/**").hasAnyRole("USER","ADMIN")
-                .anyRequest().permitAll()
-                .and().formLogin().loginPage("/login")
-                .and().logout().logoutSuccessUrl("/")
-                .permitAll()
-                .and().exceptionHandling().accessDeniedPage("/403");
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SpringDataUserDetailsService customUserDetailsService() {
+        return new SpringDataUserDetailsService();
     }
 
 
